06b0aeaaf23f62d41ac8f62ab057c59fb5eddf3b
Kira Chen <kira.chen@linux88.com>
2025-04-09 13:01:48 +0800

    feat(player): Moving the Player at a Normal Speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right);
+        // Move the player to the right at a speed of 5 units per second
+        transform.Translate(Vector3.right * Time.deltaTime * 5f);
+        // transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * 5f);
+        // transform.Translate(new Vector3(Time.deltaTime * 5f, 0, 0));
     }
 }

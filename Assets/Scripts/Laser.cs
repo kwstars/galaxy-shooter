8a2afd4d180ccacde9dfe57305d5125aed87b2e3
Kira Chen <kira.chen@linux88.com>
2025-04-09 16:18:14 +0800

    feat(Shooting): Destroy Laser

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -17,6 +17,13 @@ public class Laser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Move the laser upwards based on speed and delta time
         transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        // Destroy the laser if it goes off-screen
+        if (transform.position.y > 8f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
